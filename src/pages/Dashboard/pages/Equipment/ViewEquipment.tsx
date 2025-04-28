e985eebc814c1fbc8e61bf1fc8d38707604a1a1d
Stefan0712 <s.vladulescu@gmail.com>
2025-04-28 14:42:03 +0300

    Styled View Equipment more

--- src/pages/Dashboard/pages/Equipment/ViewEquipment.tsx
+++ src/pages/Dashboard/pages/Equipment/ViewEquipment.tsx
@@ -7,19 +7,21 @@ interface ViewEquipmentProps {
 //TODO: Add a link to the author's profile
 const ViewEquipment: React.FC<ViewEquipmentProps> = ({equipment}) => {
     return ( 
-        <div className="w-full h-full primary-color">
-            {!equipment ? <h1>Select an equipment to view</h1> : <div>
+        <div className="w-full h-full primary-color p-4">
+            {!equipment ? <h1>Select an equipment to view</h1> : <div className="flex flex-col gap-2">
                 <h2 className="full font-bold text-2xl">{equipment.name}</h2>
-                <div><p>Created by {equipment?.authorId?.username}</p><p>Created on {formatDateToPretty(equipment?.createdAt)}</p></div>
-                <p>{equipment.description || 'Description not set'}</p>
-                {equipment.url ? <a href={equipment.url} target="_blank">{equipment.urlName || equipment.url}</a> : "No references provided"}
-                <h3>Target Muscles</h3>
-                <div>
+                <p className="text-white text-opacity-50 pl-[10px]">Created by {equipment?.authorId?.username} on {formatDateToPretty(equipment?.createdAt)}</p>
+                <h3 className="font-bold text-white text-opacity-75">Description</h3>
+                <p className="pl-[10px]">{equipment.description || 'Description not set'}</p>
+                <h3 className="font-bold text-white text-opacity-75">Reference (URL)</h3>
+                {equipment.url ? <a className="pl-[10px]" href={equipment.url} target="_blank">{equipment.urlName || equipment.url}</a> : <p className="pl-[10px]">No references provided</p>}
+                <h3 className="font-bold text-white text-opacity-75">Target Muscles</h3>
+                <div className="flex pl-[10px] items-center gap-2">
                     {equipment.muscleGroups?.map((item, index)=><p key={"muscle-"+index}>{item.name}</p>)}
                 </div>
-                <h3>Tags</h3>
-                <div>
-                    {equipment.tags?.map((item, index)=><div key={"tag-"+index}><div style={{backgroundColor: item.color || 'white'}}></div><p>{item.name}</p></div>)}
+                <h3 className="font-bold text-white text-opacity-75">Tags</h3>
+                <div className="flex pl-[10px] items-center gap-2">
+                    {equipment.tags?.map((item, index)=><div key={"tag-"+index} className="flex gap-2 items-center"><div className="w-[10px] h-[10px] rounded" style={{backgroundColor: item.color || 'white'}}></div><p>{item.name}</p></div>)}
                 </div>
                 </div>}
         </div>
