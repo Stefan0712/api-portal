9e108a9cd7cba464e86bfce01ffdc4793e290c32
Stefan0712 <s.vladulescu@gmail.com>
2025-04-30 23:53:14 +0300

    Addded a drag and drop library and implemented it to the Exercises sections. Replaced the list of 'created exercises' to multiple phases, each of them being a list of exercises

--- src/pages/Workout/ExerciseList.tsx
+++ src/pages/Workout/ExerciseList.tsx
@@ -3,17 +3,19 @@ import { useState } from 'react';
 import { IconLibrary } from '../../IconLibrary';
 import { Exercise } from '../../types/interfaces';
 import axios from "../../axios";
+import { Droppable, Draggable } from '@hello-pangea/dnd';
 
 interface ExercisePickerProps {
     addExercise: (tag: Exercise) => void;
-    currentExercises: Exercise[]
+    currentExercises: Exercise[];
+    exercises: Exercise[];
+    setExercises: (exercises: Exercise[]) => void;
 }
-const ExerciseList: React.FC<ExercisePickerProps> = ({addExercise, currentExercises}) => {
+const ExerciseList: React.FC<ExercisePickerProps> = ({addExercise, currentExercises, exercises, setExercises}) => {
 
 
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [items, setItems] = useState<Exercise[]>([]);
-    const [exercises, setExercises] = useState<Exercise[]>([]);
 
     const handleGetExercises = async () =>{
         try{
@@ -47,22 +49,34 @@ const ExerciseList: React.FC<ExercisePickerProps> = ({addExercise, currentExerci
 
     //TODO: Add filters such as "My Exercises/Public Exercises/ Official Exercises", difficulty, target group.
     //TODO: Make it so that users can search by tags, target muscles, and other properties.
-    return ( 
+    return (
         <div className='flex flex-col flex-1 gap-2 h-[350px] primary-color p-2'>
-            <input className="pl-2 h-[40px] rounded secondary-color flex-shrink-0" type="text" minLength={0} maxLength={100} onChange={(e)=>handleSeach(e.target.value)} value={searchQuery} placeholder='Search existing exercises...'></input>
-            <div className="flex flex-col gap-2 flex-1 overflow-y-auto overflow-x-hidden">
-                {exercises?.length > 0 ? exercises.map((item,index)=>
-                    checkIfAdded(item) ? null : (
-                        <div className="w-full h-[40px] flex-shrink-0 flex items-center gap-4" id={item.name} key={index}>
-                            <h4>{item.name}</h4>
-                            <p className="ml-auto">{item.sets} sets</p>
-                            <button type="button" onClick={() => addExercise(item)} className="small-square transparent-bg"> <img src={IconLibrary.Add} className="w-[40px] h-[40px]" alt="" /></button>
-                        </div>
-                    )
-                ):<p>Items not found</p>}
-            </div>
+            <input className="pl-2 h-[40px] rounded secondary-color flex-shrink-0" type="text" minLength={0} maxLength={100} onChange={(e) => handleSeach(e.target.value)} value={searchQuery} placeholder='Search existing exercises...'/>
+            <Droppable droppableId="pool">
+                {(provided) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2 flex-1 overflow-y-auto overflow-x-hidden px-2">
+                        {exercises?.length > 0 ? (
+                            exercises.map((item, index) =>
+                                checkIfAdded(item) ? null : (
+                                    <Draggable draggableId={item._id} index={index} key={item._id} >
+                                        {(provided) => (
+                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="w-full h-[40px] flex-shrink-0 flex items-center gap-4">
+                                                <h4>{item.name}</h4>
+                                                <p className="ml-auto">{item.sets} sets</p>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                )
+                            )
+                        ) : (
+                            <p>Items not found</p>
+                        )}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
         </div>
-     );
+    );
 }
  
 export default ExerciseList;
--- src/pages/Workout/NewWorkout.tsx
+++ src/pages/Workout/NewWorkout.tsx
@@ -12,6 +12,7 @@ import { getUserData, isLoggedIn } from "../../utils/auth.ts";
 import ErrorLoginPage from "../common/LoginErrorPage.tsx";
 import { useMessage } from "../../context/MessageContext.tsx";
 import ExerciseList from "./ExerciseList.tsx";
+import { DragDropContext, Droppable, Draggable, } from '@hello-pangea/dnd';
 
 
 interface IPhase {
@@ -157,6 +158,34 @@ const NewWorkout: React.FC = () => {
     //get duration based on each exercise duration that will be set as workout duration if none is specified by the user
     const getTotalDuration = () => exercises.reduce((sum, exercise) => sum + (exercise.duration ?? 0), 0);
 
+    const handleDragEnd = (result) => {
+        const { source, destination, draggableId } = result;
+        if (!destination) return;
+        if (source.droppableId === destination.droppableId && source.index === destination.index) return;
+      
+        const updatedPhases = [...phases];
+        let draggedExercise;
+      
+        if (source.droppableId === 'pool') {
+            // Dragging from the exercise pool
+            const original = allExercises.find(ex => ex._id === draggableId);
+            draggedExercise = { ...original, _id: `${original._id}-${Date.now()}` }; // new _id makes it unique
+
+        } else {
+            // Dragging from a phase
+            const sourcePhaseIndex = parseInt(source.droppableId.split('-')[1]);
+            draggedExercise = updatedPhases[sourcePhaseIndex].exercises[source.index];
+            updatedPhases[sourcePhaseIndex].exercises.splice(source.index, 1);
+        }
+      
+        if (destination.droppableId !== 'pool') {
+            const destPhaseIndex = parseInt(destination.droppableId.split('-')[1]);
+            updatedPhases[destPhaseIndex].exercises.splice(destination.index, 0, draggedExercise);
+        }
+      
+        setPhases(updatedPhases);
+      };
+      
 
     
     //TODO: Add Preview Workout where the app shows the view of other users
@@ -199,7 +228,7 @@ const NewWorkout: React.FC = () => {
                                     {exerciseTags?.length > 0 ? exerciseTags.map((item)=><div key={item.name+item.color} className="h-[40px] flex gap-2 secondary-color px-2 items-center rounded flex-shrink-0"><div className="h-[15px] w-[15px] rounded" style={{backgroundColor: item.color}}></div><p>{item.name}</p><img className=" w-[20px] h-[20px]" src={IconLibrary.No} onClick={()=>setExerciseTags((exerciseTags)=>[...exerciseTags.filter(it=>it.id!==item.id)]) }/></div>) : <p className="px-2 py-1 font-bold">No Tags</p>}
                                 </div>
                             </div>
-                            {/* <div className="flex flex-col gap-2 w-1/2 h-[250px] p-3">
+                            <div className="flex flex-col gap-2 w-1/2 h-[250px] p-3">
                                 <h3 className="font-bold text-xl">Target Muscles</h3>
                                 <div className="flex gap-2">
                                     <button type="button" onClick={()=>setShowGroups(true)}><img className="w-[30px] h-[30px]" src={IconLibrary.Search} alt=""/></button>
@@ -221,48 +250,55 @@ const NewWorkout: React.FC = () => {
                                         <img className="w-[20px] h-[20px] ml-auto" src={IconLibrary.No} onClick={()=>setEquipments((equipments)=>[...equipments.filter(it=>it.id!==item.id)]) }/>
                                     </div>) : <p className="px-2 py-1 font-bold">No Equipment</p>}
                                 </div>
-                            </div> */}
-                            <div className="flex flex-col w-full h-[400px]">
-                                <h3 className="font-bold text-xl h-[50px]">Exercises</h3>
-                                <div className="grid grid-cols-[300px_1fr] gap-2 w-full overflow-hidden h-[350px]">
-                                    <ExerciseList currentExercises={exercises} addExercise={handleAddExercise} />
-                                    <div className="flex items-center gap-2 h-full overflow-x-auto overflow-y-hidden rounded">
-                                        {phases && phases.length > 0 ? phases.map((phase, index)=> <div className="primary-color w-[300px] flex-shrink-0 h-full p-1 flex flex-col gap-2" key={"Phase-"+index}>
-                                        <h2 className="font-bold border-b border-white border-opacity-20 pb-1">{phase.name}</h2>
-                                            {phase && phase.exercises.length > 0 ? exercises.map((exercise, index) => (
-                                                    <div className="w-full h-[40px] flex-shrink-0 flex items-center gap-4" id={exercise.name} key={index}>
-                                                        <h4>{exercise.name}</h4>
-                                                        <p className="ml-auto">{exercise.sets} sets</p>
-                                                        <button type="button" onClick={() => handleRemoveExercise(exercise._id)} className="small-square transparent-bg"> <img src={IconLibrary.Close} className="w-[30px] h-[30px]" alt="" /></button>
-                                                    </div>
-                                                    )
-                                                ) : (
-                                                    <h3>No exercises added</h3>
-                                                )
-                                            }
-                                        </div>) : <p>No phase created</p>}
-                                        <button type="button" onClick={()=>setPhases((phases)=>[...phases,{position: phases.length, name: "New Phase", exercises: []}])} className="flex-shrink-0 primary-color w-[100px] h-full p-1 flex flex-col items-center justify-center gap-2" key={"Phase-add"}><img src={IconLibrary.Add} className="w-[40px] h-[40px]" alt="" /></button>
-                                    </div>
-                                </div>
                             </div>
-                            {/* <div className="flex flex-col gap-2 w-full h-[300px] p-3">
-                                <h3 className="font-bold text-xl">Exercises</h3>
-                                {showExercisePicker ? <ExercisePicker closeModal={()=>setShowExercisePicker(false)} currentExercises={exercises} addExercise={handleAddExercise} /> : null}
-                                <button className="w-[150px] h-[40px] secondary-color rounded" type="button" onClick={()=>setShowExercisePicker(true)}>Add Exercise</button>
-                                <div className="flex flex-col gap-2 h-[150px] overflow-x-hidden overflow-y-auto primary-color rounded p-2 pr-[20px]">
-                                {exercises && exercises.length > 0 ? exercises.map((exercise, index) => (
-                                            <div className="w-full h-[40px] flex-shrink-0 flex items-center gap-4" id={exercise.name} key={index}>
-                                                <h4>{exercise.name}</h4>
-                                                <p className="ml-auto">{exercise.sets} sets</p>
-                                                <button type="button" onClick={() => handleRemoveExercise(exercise._id)} className="small-square transparent-bg"> <img src={IconLibrary.Close} className="w-[30px] h-[30px]" alt="" /></button>
+                            <div className="flex flex-col w-full h-[400px]">
+                                <h3 className="font-bold text-xl h-[50px]" onClick={()=>console.log(phases)}>Exercises</h3>
+                                <DragDropContext onDragEnd={handleDragEnd}>
+                                <div className="grid grid-cols-[300px_1fr] gap-2 w-full overflow-hidden h-[350px]">
+                                    <ExerciseList currentExercises={exercises} addExercise={handleAddExercise} exercises={allExercises} setExercises={(items)=>setAllExercises(items)} />
+
+                                    <div className="flex items-center gap-2 h-full overflow-x-auto overflow-y-hidden rounded">
+                                        {phases && phases.length > 0 ? (
+                                        phases.map((phase, phaseIndex) => (
+                                            <div className="primary-color w-[300px] flex-shrink-0 h-full p-1 flex flex-col gap-2">
+                                            <h2 className="font-bold border-b border-white border-opacity-20 pb-1">{phase.name}</h2>
+
+                                            <Droppable droppableId={`phase-${phaseIndex}`}>
+                                                {(provided) => (
+                                                <div ref={provided.innerRef} {...provided.droppableProps} className="flex flex-col gap-2 flex-1 overflow-y-auto">
+                                                    {phase.exercises.length > 0 ? (
+                                                    phase.exercises.map((exercise, index) => (
+                                                        
+                                                        <Draggable draggableId={exercise._id} index={index} key={'Exercise-'+index}>
+                                                        {(provided) => (
+                                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="w-full h-[40px] flex-shrink-0 flex items-center gap-4">
+                                                            <h4>{exercise.name}</h4>
+                                                            <p className="ml-auto">{exercise.sets} sets</p>
+                                                            <button type="button" onClick={() => handleRemoveExercise(exercise._id)} className="small-square transparent-bg">
+                                                                <img src={IconLibrary.Close} className="w-[30px] h-[30px]" alt="" />
+                                                            </button>
+                                                            </div>
+                                                        )}
+                                                        </Draggable>
+                                                    ))
+                                                    ) : (
+                                                    <h3>No exercises added</h3>
+                                                    )}
+                                                    {provided.placeholder}
+                                                </div>
+                                                )}
+                                            </Droppable>
                                             </div>
-                                            )
-                                        ) : (
-                                            <h3>No exercises added</h3>
-                                        )
-                                    }
-                                </div>
-                            </div> */}
+                                        )) ) : ( <p>No phase created</p> )}
+
+                                        <button type="button" onClick={() => setPhases((phases) => [...phases, { position: phases.length, name: 'New Phase', exercises: [] }])} className="flex-shrink-0 primary-color w-[100px] h-full p-1 flex flex-col items-center justify-center gap-2" key={"Phase-add"}>
+                                        <img src={IconLibrary.Add} className="w-[40px] h-[40px]" alt="" />
+                                        </button>
+                                    </div>
+                                    </div>
+
+                                </DragDropContext>
+                            </div>
                     </form>
             </div>
         );
