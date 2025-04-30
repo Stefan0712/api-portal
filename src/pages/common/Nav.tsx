5992e7cf046089e7996596095f2b77b8548221fd
Stefan0712 <s.vladulescu@gmail.com>
2025-04-30 16:56:55 +0300

    Added a 100% opacity for the nav button corresponding the current path to let users know where they are

--- src/pages/common/Nav.tsx
+++ src/pages/common/Nav.tsx
@@ -1,29 +1,32 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { isLoggedIn, logoutUser } from "../../utils/auth";
 import { IconLibrary } from "../../IconLibrary";
 
 const Nav = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
+    
 
     const handleLogout = () =>{
         logoutUser();
         navigate('/login');
     }
-
+    
     return ( 
         <nav className='navigation px-[15px] menu-color text-white text-opacity-60 flex flex-col py-[20px]'>
             <h1 className="text-xl font-bold mb-[30px]">EasyFit</h1>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Home} alt="home button" />Home</Link>
-            <Link to={'/'} className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Planner} alt="home button" />Planner</Link>
-            <Link to="/exercises" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Exercise} alt="home button" />Exercises</Link>
-            <Link to="/workouts" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Workout} alt="home button" />Workouts</Link>
-            <Link to="/equipment" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Equipment} alt="home button" />Equipment</Link>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Guides} alt="home button" />Guides</Link>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Posts} alt="home button" />Posts</Link>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Community} alt="home button" />Community</Link>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Friends} alt="home button" />Friends</Link>
-            <Link to="/" className="flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1"><img className="w-[15px] h-[15px]" src={IconLibrary.Activity} alt="home button" />My Activity</Link>
+            <p>{location.pathname}</p>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === '' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Home} alt="home button" />Home</Link>
+            <Link to={'/'} className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Planner} alt="home button" />Planner</Link>
+            <Link to="/exercises" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === '/exercises' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Exercise} alt="home button" />Exercises</Link>
+            <Link to="/workouts" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === '/workouts' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Workout} alt="home button" />Workouts</Link>
+            <Link to="/equipment" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'equipment' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Equipment} alt="home button" />Equipment</Link>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Guides} alt="home button" />Guides</Link>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Posts} alt="home button" />Posts</Link>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Community} alt="home button" />Community</Link>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Friends} alt="home button" />Friends</Link>
+            <Link to="/" className={`flex gap-2 items-center w-full h-[30px] transition-transform duration-200 hover:translate-x-1 ${location.pathname === 'asd' ? 'selected-nav-button' : ''}`}><img className="w-[15px] h-[15px]" src={IconLibrary.Activity} alt="home button" />My Activity</Link>
             
             <div className="mt-auto content-color flex flex-col rounded-xl align-center px-[15px] py-[10px] border border-gray-800">
                 {
