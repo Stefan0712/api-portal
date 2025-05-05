96ff56552bb1038267ef274fad9f672a3d8ad356
Stefan0712 <s.vladulescu@gmail.com>
2025-05-05 15:44:10 +0300

    Changed the icon from Save Workout button from a plus icon to a save disket icon

--- src/pages/Workout/Workouts.tsx
+++ src/pages/Workout/Workouts.tsx
@@ -159,7 +159,7 @@ const Workouts = () => {
                         <div className="flex gap-[20px] align-center w-full">
                             <h2 className="font-bold mb-2 text-2xl">{selectedItem.name}</h2>
                             {isUserLoggedIn && userData ? <div className="ml-auto flex gap-5">
-                                <button className="flex gap-1 items-center" onClick={handleSaveWorkout}><img className="h-[20px] w-[20px]" src={userWorkouts && userWorkouts.saved?.length > 0 ? userWorkouts?.saved.includes(selectedItem._id) ? IconLibrary.Checkmark : IconLibrary.Add : IconLibrary.Add} alt="" /></button>
+                                <button className="flex gap-1 items-center" onClick={handleSaveWorkout}><img className="h-[20px] w-[20px]" src={userWorkouts && userWorkouts.saved?.length > 0 ? userWorkouts?.saved.includes(selectedItem._id) ? IconLibrary.Checkmark : IconLibrary.Save : IconLibrary.Save} alt="" /></button>
                                 <button className="flex gap-1 items-center" onClick={handleToggleFavorite}><img className="h-[20px] w-[20px]" src={userWorkouts && userWorkouts.favorites?.length > 0 ? userWorkouts?.favorites.includes(selectedItem._id) ? IconLibrary.StarFilled : IconLibrary.StarEmpty : IconLibrary.StarEmpty} alt="" /></button>
                                     {userData.id === selectedItem.authorId || userData.role==='admin' ? (
                                         <div className="flex gap-3 items-center">
